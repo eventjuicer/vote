25690cdca36dc6b491a544ed9b3bb68b316b4406
Adam Zygadlewicz <adam@zygadlewicz.com>
2024-09-12 22:18:08 +0200

    test

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,14 @@
 
 import SignIn from "@/components/sign-in";
 import Image from "next/image";
-import { getPerson } from "@/db/test";
 import { auth } from "../auth"
 
 
 export default async function Home() {
 
-  const person = await getPerson()
   const session = await auth()
 
-  console.log({person, session})
+  console.log({session})
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
