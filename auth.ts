dea4a9c035f12b08446144d65745ca7886ecad96
Adam Zygadlewicz <adam@zygadlewicz.com>
2024-09-12 13:39:32 +0200

    fix

--- auth.ts
+++ auth.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth"
 import LinkedIn from "next-auth/providers/linkedin"
 import { KyselyAdapter } from "@auth/kysely-adapter"
-import { db } from "../../../db"
+import { db } from "@/db/db"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [LinkedIn],
