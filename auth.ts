d9f6e1b50e00adaca2ca733b8118ae5cccfcae4d
Adam Zygadlewicz <adam@zygadlewicz.com>
2024-09-12 13:31:54 +0200

    added kysely pg for auth

--- auth.ts
+++ auth.ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth"
 import LinkedIn from "next-auth/providers/linkedin"
+import { KyselyAdapter } from "@auth/kysely-adapter"
+import { db } from "../../../db"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [LinkedIn],
+  adapter: KyselyAdapter(db),
 })
