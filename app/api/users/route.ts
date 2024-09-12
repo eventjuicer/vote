3d202ed2f34229bf8d0c2c339f1e630b08701411
Adam Zygadlewicz <adam@zygadlewicz.com>
2024-09-12 21:35:07 +0200

    added shadcn

---
+++ app/api/users/route.ts
@@ -0,0 +1,28 @@
+
+import { dialect } from "@/db/db"
+import { Kysely } from "kysely"
+import { DB } from "@/db/kysely-types"
+import { waitUntil } from "@vercel/functions"
+
+
+export async function GET() {
+
+    const db = new Kysely<DB>({dialect})
+
+    const query = await db
+    .selectFrom('User')
+    .select(["name","image"])
+    .execute()
+
+
+    waitUntil(db.destroy())
+
+    return new Response(JSON.stringify(query, null, 2));
+
+}
+
+
+export const config = {
+    runtime: 'edge',
+    regions: ['fra1']
+  };
--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,15 @@
 import SignIn from "@/components/sign-in";
 import Image from "next/image";
 import { getPerson } from "@/db/test";
+import { auth } from "../auth"
+
+
 export default async function Home() {
 
   const person = await getPerson()
+  const session = await auth()
 
-  console.log(person)
+  console.log({person, session})
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
